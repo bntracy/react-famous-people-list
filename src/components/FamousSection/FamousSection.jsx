a6a8460eaa738c81cd4621c06d7afc1727a4fb37
Beth Tracy <bntracy@gmail.com>
2024-02-13 18:49:23 -0600

    input fields now clear, base functionality complete

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -39,6 +39,8 @@ function FamousSection() {
       }
     }).then(response => {
       fetchPeople();
+      setPersonName('');
+      setPersonRole('');
     }
     ).catch(error => {
       console.log('Error when adding a person', error);
@@ -50,9 +52,9 @@ function FamousSection() {
       <section className="new-person-section">
         <form onSubmit={addPerson}>
           <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => setPersonName(e.target.value)} />
+          <input id="name-input" value={famousPersonName} onChange={e => setPersonName(e.target.value)} />
           <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
+          <input id="role-input" value={famousPersonRole} onChange={e => setPersonRole(e.target.value)} />
           <button type="submit">Done</button>
         </form>
         <p>
